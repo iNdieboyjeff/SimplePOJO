c34d7decd6e2792be8a5eee6cda6489a75d3b53f
Jeff Sutton <indieboy1979@gmail.com>
2015-12-04 13:08:14 +0100

    Add copyright message

--- src/me/jeffsutton/SimplePOJO.java
+++ src/me/jeffsutton/SimplePOJO.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2015 Jeff Sutton.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package me.jeffsutton;
 
 import com.github.underscore.Function1;
@@ -17,10 +34,8 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.io.StringReader;
 import java.net.URL;
-import java.text.DateFormat;
 import java.text.NumberFormat;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.*;
 
 
@@ -38,6 +53,14 @@ public class SimplePOJO {
     }
 
     public static void main(String[] argv) {
+        String packageString = null;
+
+        for (String a : argv) {
+            if (a.startsWith("-p:")) {
+                packageString = a.substring(3);
+            }
+        }
+
         String sampleXML2 = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n" +
                 "\n" +
                 "\n" +
@@ -221,10 +244,8 @@ public class SimplePOJO {
                 "  </programme>\n" +
                 "</tv>";
 
-        ;
-
         try {
-            String r = new SimplePOJO("me.jeffsutton").generate(sampleXML2);
+            String r = new SimplePOJO(packageString).generate(sampleXML2);
             System.out.println(r);
         } catch (Exception eek) {
             eek.printStackTrace();
